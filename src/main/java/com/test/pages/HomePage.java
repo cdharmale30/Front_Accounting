0f4593a7b6a70948f93b872a66559926c2c6d733
cdharmale30 <36369070+cdharmale30@users.noreply.github.com>
2018-12-25 08:57:30 -0800

    Second commit

--- src/main/java/com/test/pages/HomePage.java
+++ src/main/java/com/test/pages/HomePage.java
@@ -1,11 +1,51 @@
 package com.test.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.testbase.TestBase;
 
-public class HomePage extends TestBase{
-	
-	
-	
-	
+public class HomePage extends TestBase {
+
+	@FindBy(xpath = "//a[contains(text(),'urchases')]")
+	WebElement purchasesLink;
+
+	@FindBy(xpath = "//a[contains(text(),'tems and Inventory')]")
+	WebElement temsandInventoryLink;
+
+	@FindBy(xpath = "//a[contains(text(),anufacturing)]")
+	WebElement manufacturingLink;
+
+	@FindBy(xpath = "//a[contains(text(),'ixed Assets')]")
+	WebElement fixedAssetLink;
+
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	public String verifyHomePageTitle() {
+		return driver.getTitle();
+	}
+
+	public PurchasePage clickOnPurchasesLink() {
+		purchasesLink.click();
+		return new PurchasePage();
+	}
+
+	public TestAndInventoryPage clickOnTestAndInventoryLink() {
+		temsandInventoryLink.click();
+		return new TestAndInventoryPage();
+	}
+
+	public ManufacturingPage clickOnManufacturingPageLink() {
+		manufacturingLink.click();
+		return new ManufacturingPage();
+	}
+
+	public FixedAssetLinkPage clickOnFixedAssetLinkLink() {
+		fixedAssetLink.click();
+		return new FixedAssetLinkPage();
+	}
 
 }
--- src/test/java/com/test/testcases/HomePageTest.java
+++ src/test/java/com/test/testcases/HomePageTest.java
@@ -1,27 +1,32 @@
 package com.test.testcases;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.test.pages.FixedAssetLinkPage;
 import com.test.pages.HomePage;
 import com.test.pages.LoginPage;
+import com.test.pages.ManufacturingPage;
+import com.test.pages.PurchasePage;
+import com.test.pages.TestAndInventoryPage;
 import com.test.testutil.TestUtils;
 import com.testbase.TestBase;
 
-public class HomePageTest extends TestBase{
+public class HomePageTest extends TestBase {
 	LoginPage loginPage;
 	HomePage homePage;
 	TestUtils testUtil;
-
+	PurchasePage purchasePage;
+	TestAndInventoryPage  testAndInventoryPage;
+	ManufacturingPage    manufacturingPage  ;  
+	FixedAssetLinkPage fixedAssetLinkPage;
+	
 	public HomePageTest() {
 		super();
 	}
 
-	//test cases should be separated -- independent with each other
-	//before each test case -- launch the browser and login
-	//@test -- execute test case
-	//after each test case -- close the browser
-	
 	@BeforeMethod
 	public void setUp() {
 		initialization();
@@ -30,8 +35,45 @@ public class HomePageTest extends TestBase{
 		homePage = loginPage.loginPageData(pr.getProperty("username"), pr.getProperty("password"));
 	}
 
-	@Test
-	public void brokenLinkTest() {
-		
+	/*
+	 * @Test(priority=1) public static int brokenLinkCount(){ int homePageLinks =
+	 * homePage.bro Assert.assertEquals(homePageTitle,
+	 * "CRMPRO","Home page title not matched"); }
+	 */
+
+	@Test(priority = 1)
+	public void verifyHomePageTitleTest() {
+		String homePageTitle = homePage.verifyHomePageTitle();
+		Assert.assertEquals(homePageTitle, "Main Menu", "Home page title not matched");
 	}
+
+	@Test(priority = 2)
+	public void verifyPurchaseLinkTest() {
+
+		purchasePage = homePage.clickOnPurchasesLink();
+	}
+	
+	@Test(priority=3)
+	public void verifytestAndInventoryTest(){
+		
+		testAndInventoryPage = homePage.clickOnTestAndInventoryLink();
+	}
+	
+	@Test(priority=4)
+	public void verifyManufacturingLinkTest(){
+		
+		manufacturingPage = homePage.clickOnManufacturingPageLink();
+	}
+	
+	@Test(priority=5)
+	public void verifyFixedAssetLinkLinkTest() {
+		fixedAssetLinkPage=homePage.clickOnFixedAssetLinkLink();
+	}
+	
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
 }
